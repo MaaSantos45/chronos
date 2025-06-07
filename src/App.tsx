d86595afc2336236cc73d8aece50a39b762cfd23
MaaSantos45 <maasantos776@gmail.com>
2025-06-07 01:30:18 -0300

    adicionado componente DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import {Container, ContainerFluid} from "./components/Container";
 import {Heading} from "./components/Heading";
 import {Logo} from "./components/Logo";
 import {CountDown} from "./components/CountDown"
+import {Menu} from "./components/Menu";
+import {Form} from "./components/Form";
 
 import './styles/theme.css'
 import './styles/global.css'
 import './styles/App.css'
-import {Menu} from "./components/Menu";
 
 
 function App() {
@@ -25,6 +26,10 @@ function App() {
 
             <Container>
                 <CountDown />
+            </Container>
+
+            <Container>
+                <Form />
             </Container>
 
             <Container>
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import styles from './styles.module.css'
+
+type DefaultInputProps = {
+    id: string;
+    label?: string;
+} & React.ComponentProps<'input'>
+
+export function DefaultInput({id, type, label, ...rest}: DefaultInputProps) {
+    return (
+        <>
+            {label && (<label htmlFor={id}>{label}</label>)}
+            <input className={styles.input} id={id} type={type} {...rest} />
+        </>
+    )
+}
