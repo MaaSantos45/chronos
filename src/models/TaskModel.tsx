4df77bdc00274a1fe9e195ab7ba9c0689a379a7f
MaaSantos45 <maasantos776@gmail.com>
2025-06-08 00:11:42 -0300

    adicionado o modelo dos estados das tasks

---
+++ src/models/TaskModel.tsx
@@ -0,0 +1,12 @@
+import { TaskStateModel } from './TaskStateModel.tsx'
+
+export type TaskModel = {
+    id: string,
+    name: string,
+    duration: number,
+    startDate: number,
+    completeDate: number | null,
+    interupted: boolean,
+    type: keyof TaskStateModel['config'];
+};
+
---
+++ src/models/TaskStateModel.tsx
@@ -0,0 +1,17 @@
+import { TaskModel } from "./TaskModel";
+
+export type TaskStateModel = {
+    tasks: TaskModel[];
+
+    secondsRemaining: number;
+    formatedSecondsRemaining: string;
+    activeTask: TaskModel | null;
+    currentCycle: number
+
+    config: {
+        "workTime": number;
+        "breakTime": number;
+        "restTime": number;
+    }
+};
+
